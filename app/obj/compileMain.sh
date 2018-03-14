e49c3bb168dd9c71fcd2af4ef1fd7080f6c369bd
remynotmartin <remy_dinh@yahoo.com>
2018-03-14 01:10:38 -0700

    Changed compile script to only make object files

--- app/obj/compileMain.sh
+++ app/obj/compileMain.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -std=c++11 -Wall ../src/*.cpp -I ../../libs/include -L../../lib -lnamegen -o ../../bin/codeTalker.out
+g++ -std=c++11 -Wall -c ../src/*.cpp -I ../../libs/include -L../../lib -lnamegen -o ../../bin/codeTalker.out
